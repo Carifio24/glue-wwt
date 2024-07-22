ef723423ecb809f193b3d75f466f47594aa49b4b
Carifio24 <carifio24@gmail.com>
2024-07-22 15:34:12 -0400

    Update __init__ to make Qt optional.

--- glue_wwt/__init__.py
+++ glue_wwt/__init__.py
@@ -1,12 +1,22 @@
 from __future__ import absolute_import, division, print_function
 
-# The following needs to be imported before the application is constructed
-from qtpy.QtWebEngineWidgets import QWebEnginePage  # noqa
+try:
+    # The following needs to be imported before the application is constructed
+    from qtpy.QtWebEngineWidgets import QWebEnginePage  # noqa
+except ImportError:
+    pass
 
 from .version import __version__  # noqa
 
 
-def setup():
+def setup_qt():
     from .viewer.qt_data_viewer import WWTQtViewer
     from glue.config import qt_client
     qt_client.add(WWTQtViewer)
+
+
+def setup():
+    try:
+        setup_qt()
+    except:
+        pass
